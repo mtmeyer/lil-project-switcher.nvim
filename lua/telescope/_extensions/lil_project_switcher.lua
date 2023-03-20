d9499a5130000f926570d2d05d31672192ed3f35
Michael Meyer <michael@mtmeyer.net>
2023-03-20 19:29:40 +1100

    chore: test simple finder

--- lua/telescope/_extensions/lil_project_switcher.lua
+++ lua/telescope/_extensions/lil_project_switcher.lua
@@ -1,11 +1,18 @@
 local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+
+local test = {
+    "abc",
+    "def",
+}
 
 local run = function(opts)
     opts = opts or {}
     opts.cwd = "~/Documents/git"
 
     local picker = pickers.new(opts, {
-        prompt_title = "Project switcher"
+        prompt_title = "Project switcher",
+        finder = finders.new_table({ results = test})
     })
 
     return picker:find()
