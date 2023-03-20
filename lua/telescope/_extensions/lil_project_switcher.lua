2f8de203e48110c58fc3e1e354fda94d3ee0c8b8
Michael Meyer <michael@mtmeyer.net>
2023-03-20 20:37:54 +1100

    feat: add ability to pass in options

--- lua/telescope/_extensions/lil_project_switcher.lua
+++ lua/telescope/_extensions/lil_project_switcher.lua
@@ -2,10 +2,11 @@ local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 
 
-local scandir = function(directory)
+local scandir = function(directory, maxdepth)
+    maxdepth = maxdepth or 1
     local popen = io.popen
     local i, dirs = 0, {}
-    local pfile = popen( "find " .. directory ..  " -maxdepth 1 -type d")
+    local pfile = popen( "find " .. directory ..  " -maxdepth " .. maxdepth .. " -type d")
     for filename in pfile:lines() do
         i = i + 1
         dirs[i] = filename
@@ -17,10 +18,12 @@ end
 
 local run = function(opts)
     opts = opts or {}
+    opts.maxdepth = opts.maxdepth or 1
+    opts.directory = opts.directory or '~'
 
     local picker = pickers.new(opts, {
         prompt_title = "Project switcher",
-        finder = finders.new_table({ results = scandir('~/Documents/git')})
+        finder = finders.new_table({ results = scandir(opts.directory, opts.maxdepth)})
     })
 
     return picker:find()
