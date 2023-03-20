b3e0dcda86df8da89b53b551c5295dfceef59dc8
Michael Meyer <michael@mtmeyer.net>
2023-03-20 21:08:42 +1100

    chore: fix how extension config is passed through

--- lua/telescope/_extensions/lil_project_switcher.lua
+++ lua/telescope/_extensions/lil_project_switcher.lua
@@ -1,14 +1,6 @@
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
-local utils = require "telescope._extensions.utils"
-
-local options = {}
-
-local default_options = {
-    directory = '~',
-    maxdepth = 1,
-}
-
+local conf = require('telescope.config').values
 
 local scandir = function(directory, maxdepth)
     maxdepth = maxdepth or 1
@@ -23,21 +15,27 @@ local scandir = function(directory, maxdepth)
     return dirs
 end
 
-
 local run = function(opts)
     opts = opts or {}
-
+    local directory = conf.directory or '~'
+    local maxdepth = conf.maxdepth or 1
     local picker = pickers.new(opts, {
         prompt_title = "Project switcher",
-        finder = finders.new_table({ results = scandir(options.directory, options.maxdepth)})
+        finder = finders.new_table({ results = scandir(directory, maxdepth)})
     })
 
     return picker:find()
 end
 
 return require("telescope").register_extension({
-    setup = function(ext_config, _)
-        options = utils.assign({}, default_options, ext_config)
+    setup = function(ext_config, config)
+        if ext_config.directory then
+            config.directory = ext_config.directory
+        end
+
+        if ext_config.maxdepth then
+            config.maxdepth = ext_config.maxdepth
+        end
     end,
     exports = {
         lil_project_switcher = run
