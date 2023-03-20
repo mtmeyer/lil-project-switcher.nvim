51a5708daa0680a72348d9f6419b8aed3d8704fb
Michael Meyer <michael@mtmeyer.net>
2023-03-20 21:34:57 +1100

    chore: tweaks

--- lua/telescope/_extensions/lil_project_switcher.lua
+++ lua/telescope/_extensions/lil_project_switcher.lua
@@ -1,43 +1,42 @@
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
-local conf = require('telescope.config').values
+local conf = require("telescope.config").values
 
 local scandir = function(directory, maxdepth)
-    maxdepth = maxdepth or 1
-    local popen = io.popen
-    local i, dirs = 0, {}
-    local pfile = popen( "find " .. directory ..  " -maxdepth " .. maxdepth .. " -type d")
-    for filename in pfile:lines() do
-        i = i + 1
-        dirs[i] = filename
-    end
+	local popen = io.popen
+	local i, dirs = 0, {}
+	local pfile = popen("find " .. directory .. " -maxdepth " .. maxdepth .. " -type d")
+	for filename in pfile:lines() do
+		i = i + 1
+		dirs[i] = filename
+	end
 
-    return dirs
+	return dirs
 end
 
 local run = function(opts)
-    opts = opts or {}
-    local directory = conf.directory or '~'
-    local maxdepth = conf.maxdepth or 1
-    local picker = pickers.new(opts, {
-        prompt_title = "Project switcher",
-        finder = finders.new_table({ results = scandir(directory, maxdepth)})
-    })
+	opts = opts or {}
+	local directory = conf.directory or "~"
+	local maxdepth = conf.maxdepth or 1
+	local picker = pickers.new(opts, {
+		prompt_title = "Project switcher",
+		finder = finders.new_table({ results = scandir(directory, maxdepth) }),
+	})
 
-    return picker:find()
+	return picker:find()
 end
 
 return require("telescope").register_extension({
-    setup = function(ext_config, config)
-        if ext_config.directory then
-            config.directory = ext_config.directory
-        end
+	setup = function(ext_config, config)
+		if ext_config.directory then
+			config.directory = ext_config.directory
+		end
 
-        if ext_config.maxdepth then
-            config.maxdepth = ext_config.maxdepth
-        end
-    end,
-    exports = {
-        lil_project_switcher = run
-    }
+		if ext_config.maxdepth then
+			config.maxdepth = ext_config.maxdepth
+		end
+	end,
+	exports = {
+		lil_project_switcher = run,
+	},
 })
