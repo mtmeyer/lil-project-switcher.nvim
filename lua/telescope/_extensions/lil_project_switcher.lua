96d260b2efe51d728b4d7bbc33e9d4994a53d9cc
Michael Meyer <michael@mtmeyer.net>
2023-03-21 01:01:23 +1100

    feat: add ability for multiple directories to be added into the config

--- lua/telescope/_extensions/lil_project_switcher.lua
+++ lua/telescope/_extensions/lil_project_switcher.lua
@@ -7,31 +7,44 @@ local get_directory_name = function(directory, filename)
 	return filename:gsub(home_dir .. "/", "")
 end
 
-local scandir = function(directory, maxdepth)
+local scandir = function(directory, maxdepth, name)
 	local popen = io.popen
 	local i, dirs = 0, {}
 	local pfile = popen("find " .. directory .. " -maxdepth " .. maxdepth .. " -type d")
 	for filename in pfile:lines() do
 		i = i + 1
-		dirs[i] = { filename, get_directory_name(directory, filename) }
+		local tmp_object = { value = filename, display = name .. ": " .. get_directory_name(directory, filename) }
+		dirs[i] = tmp_object
 	end
 
 	return dirs
 end
 
+local get_results_for_directory = function(directories, maxdepth)
+	if type(directories) == "table" then
+		local tmp_table = {}
+		for _, value in ipairs(directories) do
+			tmp_table = vim.tbl_deep_extend("keep", tmp_table, scandir(value.directory, maxdepth, value.name))
+		end
+		return tmp_table
+	else
+		error("Config error: directories must be a table. Refer to the readme for correct format.")
+	end
+end
+
 local run = function(opts)
 	opts = opts or {}
-	local directory = conf.directory or "~"
 	local maxdepth = conf.maxdepth or 1
+	local results = get_results_for_directory(conf.directories, maxdepth) or "~"
 	local picker = pickers.new(opts, {
 		prompt_title = "Project switcher",
 		finder = finders.new_table({
-			results = scandir(directory, maxdepth),
+			results = results,
 			entry_maker = function(entry)
 				return {
-					value = entry[1],
-					display = entry[2],
-					ordinal = entry[2],
+					value = entry.value,
+					display = entry.display,
+					ordinal = entry.display,
 				}
 			end,
 		}),
@@ -42,8 +55,8 @@ end
 
 return require("telescope").register_extension({
 	setup = function(ext_config, config)
-		if ext_config.directory then
-			config.directory = ext_config.directory
+		if ext_config.directories then
+			config.directories = ext_config.directories
 		end
 
 		if ext_config.maxdepth then
